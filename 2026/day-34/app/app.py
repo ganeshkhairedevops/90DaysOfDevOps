0f42fccb9990acda317ba195b13444b644789e4f
ganeshkhairedevops <ganeshkhaire14@gmail.com>
2026-02-27 13:58:51 +0530

    add changes

--- 2026/day-34/app/app.py
+++ 2026/day-34/app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask + MySQL + Redis!"
+    return "Hello from Flask + MySQL + Redis! updated"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
