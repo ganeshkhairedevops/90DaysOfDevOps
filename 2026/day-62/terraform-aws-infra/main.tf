b173c84a809fd834b9275a8fcfa5f6b0ac960481
ganeshkhairedevops <ganeshkhaire14@gmail.com>
2026-04-02 17:58:01 +0530

    add sg

--- 2026/day-62/terraform-aws-infra/main.tf
+++ 2026/day-62/terraform-aws-infra/main.tf
@@ -53,4 +53,53 @@ resource "aws_route_table_association" "public_rt_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+# Security Group
+# Firewall: allow SSH & HTTP, all outbound
+resource "aws_security_group" "ec2-sg" {
+  name        = "TerraWeek-SG"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.vpc.id
 
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Name" = "TerraWeek-SG"
+  }
+}
+
+# EC2 Instance
+# Launch a server in public subnet
+resource "aws_instance" "ec2" {
+  ami                         = "ami-01b14b7ad41e17ba4"
+  instance_type               = "t2.micro"
+  key_name                    = "terraserver"
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.ec2-sg.id]
+
+  tags = {
+    "Name" = "TerraWeek-Server"
+  }
+}
