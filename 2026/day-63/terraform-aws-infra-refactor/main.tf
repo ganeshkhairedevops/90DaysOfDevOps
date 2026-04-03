6cf812d98f57d34476ba19e4148c5d60d383990e
ganeshkhairedevops <ganeshkhaire14@gmail.com>
2026-04-03 14:55:08 +0530

    add main

--- 2026/day-63/terraform-aws-infra-refactor/main.tf
+++ 2026/day-63/terraform-aws-infra-refactor/main.tf
@@ -100,7 +100,7 @@ resource "aws_instance" "ec2" {
   vpc_security_group_ids      = [aws_security_group.ec2-sg.id]
 
   tags = {
-    "Name" = var.project_name.var.environment
+    "Name" = "${var.project_name}-${var.environment}"
   }
   lifecycle {
   create_before_destroy = true
