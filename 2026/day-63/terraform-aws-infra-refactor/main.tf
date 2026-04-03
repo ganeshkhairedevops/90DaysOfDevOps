2004206a6cfe02249ee43298e2166254c6686d82
ganeshkhairedevops <ganeshkhaire14@gmail.com>
2026-04-03 16:44:49 +0530

    fix public dns issue

--- 2026/day-63/terraform-aws-infra-refactor/main.tf
+++ 2026/day-63/terraform-aws-infra-refactor/main.tf
@@ -12,6 +12,8 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = var.subnet_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   #map_public_ip_on_launch = true
 
   tags = {
