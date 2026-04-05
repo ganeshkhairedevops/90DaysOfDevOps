9406d2987a674a3d37a03bec6589b6de8600e74f
ganeshkhairedevops <ganeshkhaire14@gmail.com>
2026-04-05 18:55:43 +0530

    add tf state

---
+++ 2026/day-64/terraform-state-management/provider.tf
@@ -0,0 +1,14 @@
+# terraform Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region = var.region
+}
