2692236c24ac1e0cabb311e824582a9deb5da82d
ganeshkhairedevops <ganeshkhaire14@gmail.com>
2026-04-06 18:35:47 +0530

    check issue

--- 2026/day-65/terraform-modules/modules/security-group/main.tf
+++ 2026/day-65/terraform-modules/modules/security-group/main.tf
@@ -1,7 +1,6 @@
 resource "aws_security_group" "sg" {
     name = var.sg_name
     vpc_id = var.vpc_id
-    tags = var.tags
     description = "Security group with dynamic allowed ports"
 
     dynamic "ingress" {
