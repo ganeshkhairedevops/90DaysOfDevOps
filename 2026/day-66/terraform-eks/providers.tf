98fa16bcd68f1971817e608e7aacfbc23bd31319
ganeshkhairedevops <ganeshkhaire14@gmail.com>
2026-04-08 11:36:22 +0530

    add variables and pvc and provider

---
+++ 2026/day-66/terraform-eks/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+provider "aws" {
+    region = var.region
+  
+}
